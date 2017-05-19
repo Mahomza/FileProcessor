da4788d0316cac6099380fc863cfe0d497208a50
Mahomza <alex.prijojo.prince@gmail.com>
2017-05-19 15:08:40 +0200

    Added file processing project

---
+++ File_ProcessingFrm.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace FileProcessTestClient_APMahomana
+{
+    partial class File_ProcessingFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.processFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtfilepath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.Label();
+            this.txtFirstFile = new System.Windows.Forms.Label();
+            this.txtSecondFile = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.processFileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(499, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // processFileToolStripMenuItem
+            // 
+            this.processFileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.processFileToolStripMenuItem.Name = "processFileToolStripMenuItem";
+            this.processFileToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.processFileToolStripMenuItem.Text = "Process File";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcess.Location = new System.Drawing.Point(369, 83);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 1;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source File:";
+            // 
+            // txtfilepath
+            // 
+            this.txtfilepath.Location = new System.Drawing.Point(102, 45);
+            this.txtfilepath.Name = "txtfilepath";
+            this.txtfilepath.Size = new System.Drawing.Size(357, 20);
+            this.txtfilepath.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Results:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(369, 266);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.AutoSize = true;
+            this.txtResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResults.Location = new System.Drawing.Point(99, 146);
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(0, 13);
+            this.txtResults.TabIndex = 7;
+            // 
+            // txtFirstFile
+            // 
+            this.txtFirstFile.AutoSize = true;
+            this.txtFirstFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstFile.Location = new System.Drawing.Point(99, 185);
+            this.txtFirstFile.Name = "txtFirstFile";
+            this.txtFirstFile.Size = new System.Drawing.Size(74, 13);
+            this.txtFirstFile.TabIndex = 8;
+            this.txtFirstFile.Text = "First File Path*";
+            // 
+            // txtSecondFile
+            // 
+            this.txtSecondFile.AutoSize = true;
+            this.txtSecondFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSecondFile.Location = new System.Drawing.Point(99, 231);
+            this.txtSecondFile.Name = "txtSecondFile";
+            this.txtSecondFile.Size = new System.Drawing.Size(92, 13);
+            this.txtSecondFile.TabIndex = 9;
+            this.txtSecondFile.Text = "Second File Path*";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(257, 83);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(91, 23);
+            this.btnBrowse.TabIndex = 10;
+            this.btnBrowse.Text = "Browse File";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // File_ProcessingFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(499, 326);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtSecondFile);
+            this.Controls.Add(this.txtFirstFile);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtfilepath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "File_ProcessingFrm";
+            this.Text = "File_ProcessingFrm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.File_ProcessingFrm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem processFileToolStripMenuItem;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtfilepath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label txtResults;
+        private System.Windows.Forms.Label txtFirstFile;
+        private System.Windows.Forms.Label txtSecondFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnBrowse;
+    }
+}
---
+++ File_ProcessingFrm.cs
@@ -0,0 +1,424 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using log4net;
+using System.Configuration;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Microsoft.WindowsAPICodePack.ApplicationServices;
+
+namespace FileProcessTestClient_APMahomana
+{
+   /*
+    * Full Names: Alex Prince
+    *    Surname: Mahomana
+    *    Cell NO: 072 071 1650
+    
+    */
+    public partial class File_ProcessingFrm : Form
+    {
+        protected static readonly ILog _ilog = LogManager.GetLogger(typeof(String));
+
+        static string filePath = "";
+        static string logPath = "";
+        string sourceFile = "";
+
+        public File_ProcessingFrm()
+        {
+            InitializeComponent();
+
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            ProcessFile1();
+
+        }
+
+        public void ProcessFile1()
+        {
+            try
+            {
+                filePath = ConfigurationSettings.AppSettings["filePath"].ToString();
+                logPath = ConfigurationSettings.AppSettings["logPath"].ToString();
+
+                CreateDirectory(logPath);
+                sourceFile = txtfilepath.Text;
+
+                log4net.Config.XmlConfigurator.Configure();
+                _ilog.Info("File processing start date and time: " + DateTime.Now);
+
+                CreateDirectory(filePath);
+
+                string fileName = "SortedByFrequencyDescendingAndAlphabeticallyAscending.txt";
+
+                filePath = filePath + fileName;
+
+                StreamWriter writer = new StreamWriter(filePath);
+
+                //Create new intance of DataTable 'dtlistNameSurname'
+                DataTable dtlistNameSurname = new DataTable();
+
+                // Read CSV File and Store data to DataTable
+                dtlistNameSurname = ConvertCSVtoDataTable(sourceFile);
+
+                _ilog.Info("File 1 - Writing data to: " + filePath);
+
+
+                int totalRecords = 1;
+
+
+                //Create Array listNameSurname
+                String[] listNameSurname = new String[dtlistNameSurname.Rows.Count * 2];
+
+                //Declare index variable and assign ZERO
+                int index = 0;
+
+                //Loop through DataTable 'dtlistNameSurname' while adding to Array 'listNameSurname'
+                foreach (DataRow drTransaction in dtlistNameSurname.Rows)
+                {
+                    listNameSurname[index] = drTransaction["FirstName"].ToString();
+                    index++;
+                    listNameSurname[index] = drTransaction["LastName"].ToString();
+                    index++;
+                }
+
+                //Create new intance of dictionary
+                Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                //Loop through array, count occurrence of string while adding to dictionary
+                foreach (string word in listNameSurname)
+                {
+                    if (dictionary.ContainsKey(word))
+                    {
+                        dictionary[word] += 1;
+                    }
+                    else
+                    {
+                        dictionary.Add(word, 1);
+                    }
+                }
+
+                //Sort dictionary data by frequency descending and assign to 'sortedDict' object
+                var sortedDict = from entry in dictionary orderby entry.Value descending select entry;
+
+                //Declare new instance of dictionary
+                Dictionary<string, int> dictionaryV2 = new Dictionary<string, int>();
+
+                _ilog.Info("First and Last name ordered by frequency descending and then alphabetically ascending:");
+                //Loop through sortedDict, write all string which its occurrence is greater than ZERO
+                //Add strings which its occurrance is equal to ONE to dictionaryV2
+                foreach (var item in sortedDict)
+                {
+                    if (item.Value > 1)
+                    {
+                        writer.Write(item.Key.ToString() + ",");
+                        writer.Write(item.Value.ToString());
+                        writer.WriteLine();
+
+                        _ilog.Info(item.Key.ToString() + "," + item.Value.ToString());
+                    }
+                    else
+                    {
+                        dictionaryV2.Add(item.Key.ToString(), item.Value);
+                    }
+
+                }
+
+                //Sort dictionaryV2 data by Ascending and assign to sortedAsc
+                var sortedAsc = from entry in dictionaryV2 orderby entry.Key ascending select entry;
+
+                //Loop through sortedAsc, write all data to .txt file
+                foreach (var item in sortedAsc)
+                {
+                    writer.Write(item.Key.ToString() + ",");
+                    writer.Write(item.Value.ToString());
+                    writer.WriteLine();
+
+                    _ilog.Info(item.Key.ToString() + "," + item.Value.ToString());
+
+                }
+
+
+                _ilog.Info("Completed writing data to: " + fileName);
+                writer.Close();
+
+                txtFirstFile.Text = "Text File 1 path: " + filePath;
+                ProcessFile2(dtlistNameSurname);
+            }
+            catch (Exception ex)
+            {
+                _ilog.Info("Error: " + ex.Message);
+
+                txtResults.Text = "Error: " + ex.Message;
+
+            }
+        }
+
+        public void ProcessFile2(DataTable datatable)
+        {
+            try
+            {
+                filePath = ConfigurationSettings.AppSettings["filePath"].ToString();
+                logPath = ConfigurationSettings.AppSettings["logPath"].ToString();
+
+                sourceFile = txtfilepath.Text;
+                         
+                CreateDirectory(logPath);
+
+                log4net.Config.XmlConfigurator.Configure();
+
+                CreateDirectory(filePath);
+
+                string fileName2 = "SortedAlphabeticallyByStreetNames.txt";
+
+                filePath = filePath + fileName2;
+
+                StreamWriter writer = new StreamWriter(filePath);
+
+
+                _ilog.Info("File 2 - Writing data to: " + filePath);
+
+                int totalRecords = 1;
+
+                //Create new intance of DataTable 'dtlistNameSurname'
+                DataTable dtlistNameSurname = new DataTable();
+
+                // Read CSV File and Store data to DataTable
+                dtlistNameSurname = datatable;
+
+                //Create Array listNameSurname
+                List<AddressInfo> listAddressInfo = new List<AddressInfo>();
+
+                //Declare index variable and assign ZERO
+                int index = 0;
+                AddressInfo address = null;
+                string value = string.Empty;
+
+                //Loop through DataTable 'dtlistNameSurname' while adding to Array 'listNameSurname'
+                foreach (DataRow drTransaction in dtlistNameSurname.Rows)
+                {
+                    address = new AddressInfo();
+                    value = drTransaction["Address"].ToString();
+
+                    string[] datastr = value.Split(' ');
+                    int size = datastr.Count() - 1;
+                    for (int i = 0; i <= size; i++)
+                    {
+                        if (i == 0)
+                        {
+                            address.strNo = datastr[i].ToString();
+                        }
+                        else if (i == 1)
+                        {
+                            address.strName = datastr[i].ToString();
+                        }
+                        else if (i == 2)
+                        {
+                            address.strAbbreviation = datastr[i].ToString();
+                        }
+
+                    }
+
+                    if (address != null)
+                    {
+                        listAddressInfo.Add(address);
+                    }
+
+
+
+                }
+
+                //Create new intance of dictionary
+                Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+                //Loop through array, count occurrence of string while adding to dictionary
+                int listAddressInfoSize = listAddressInfo.Count() - 1;
+                for (int i = 0; i <= listAddressInfoSize; i++)
+                {
+                    AddressInfo word = listAddressInfo[i];
+                    if (word != null)
+                    {
+                        dictionary.Add(word.strNo, word.strName);
+                    }
+                }
+
+
+                //Sort dictionary data by frequency ascending and assign to 'sortedDict' object
+                var sortedDict = from entry in dictionary orderby entry.Value ascending select entry;
+
+                _ilog.Info("Addresses sorted aphabetically by street names:");
+
+                //Loop through sortedDict, write all string which its occurrence is greater than ZERO
+                //Add strings which its occurrance is equal to ONE to dictionaryV2
+                foreach (var item in sortedDict)
+                {
+
+                    writer.Write(item.Key.ToString() + " ");
+                    writer.Write(item.Value.ToString() + " ");
+
+                    foreach (AddressInfo word in listAddressInfo)
+                    {
+
+                        if (word.strNo == item.Key.ToString() && word.strName == item.Value.ToString())
+                        {
+                            writer.Write(word.strAbbreviation + " ");
+
+
+                            _ilog.Info(item.Key.ToString() + " " + item.Value.ToString() + " " + word.strAbbreviation);
+                            break;
+                        }
+
+                    }
+
+                    writer.WriteLine();
+
+
+
+                }
+
+
+                _ilog.Info("Completed writing data to: " + fileName2);
+                writer.Close();
+
+
+
+
+
+                #region Tracing
+                _ilog.Info("Execution finished, application will now exit");
+                _ilog.Info("File splitting successfully completed");
+
+                #endregion
+
+                txtResults.Text = "Successful completed! Please follow below directories to see generated files!";
+                txtSecondFile.Text = "Text File 2 path: " + filePath;
+
+            }
+            catch (Exception ex)
+            {
+                _ilog.Info("Error: " + ex.Message);
+                txtResults.Text = "Error: " + ex.Message;
+
+            }
+        }
+
+        public static DataTable ConvertCSVtoDataTable(string strFilePath)
+        {
+
+            _ilog.Info("Started reading file " + strFilePath);
+            _ilog.Info("Data extracted from CSV file:");
+            DataTable dt = new DataTable();
+            using (StreamReader sr = new StreamReader(strFilePath))
+            {
+                string[] headers = sr.ReadLine().Split(',');
+                foreach (string header in headers)
+                {
+                    dt.Columns.Add(header);
+                }
+                while (!sr.EndOfStream)
+                {
+                    string[] rows = sr.ReadLine().Split(',');
+                    DataRow dr = dt.NewRow();
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dr[i] = rows[i];
+                        _ilog.Info(rows[i]);
+                    }
+
+                    dt.Rows.Add(dr);
+
+
+                }
+
+            }
+
+            _ilog.Info("Reading file successful completed.");
+            return dt;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Select file";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.FileName = txtfilepath.Text;
+            fdlg.Filter = "Text and CSV Files(*.txt, *.csv)|*.txt;*.csv|Text Files(*.txt)|*.txt|CSV Files(*.csv)|*.csv|All Files(*.*)|*.*";
+            fdlg.FilterIndex = 1;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                txtfilepath.Text = fdlg.FileName;
+              
+            }
+
+            if (txtfilepath.Text !=string.Empty)
+            {
+                btnProcess.Enabled = true;
+            }
+           
+
+        }
+
+        public void CreateDirectory(string strpath)
+        {
+        
+            string path = strpath;
+
+            try
+            {
+                // Determine whether the directory exists.
+                if (!Directory.Exists(path))
+                {
+                    _ilog.Info("Started creating directory:");
+                    _ilog.Info("Directory: "+path);
+                    Directory.CreateDirectory(path);
+                    _ilog.Info("Directory successful created.");
+                }
+
+               
+            }
+            catch (Exception e)
+            {
+                _ilog.Info("Directory creation failed.");
+            }
+            
+        }
+
+        private void File_ProcessingFrm_Load(object sender, EventArgs e)
+        {
+            txtfilepath.Enabled = false;
+
+            if (txtfilepath.Text== string.Empty)
+            {
+                btnProcess.Enabled = false;
+            }
+        }
+    }
+
+    public class AddressInfo
+    {
+        public string strNo { set; get; }
+        public string strName { set; get; }
+        public string strAbbreviation { set; get; }
+    }
+
+
+}
